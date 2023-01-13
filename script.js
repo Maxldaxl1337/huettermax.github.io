858a0fd6d8d0dc10d961d4f69bbd0c640a97bb9e
Maximilian <maxihuetter123@gmail.com>
2023-01-13 12:06:41 +0100

    v0.12

--- script.js
+++ script.js
@@ -1,39 +1,28 @@
 
-document.getElementById("github").style.cursor = "pointer";
-github.addEventListener("click", (e) => {
-    window.open("https://github.com/Maxldaxl1337");
+ document.getElementById("github").style.cursor = "pointer";
+ github.addEventListener("click", (e) => {
+     window.open("https://github.com/Maxldaxl1337");
+ }) 
+
+document.getElementById("instagram").style.cursor = "pointer";
+instagram.addEventListener("click", (e) => {
+     window.open("https://www.instagram.com/maxl.htr/");
+ }) 
+
+document.getElementById("linkedin").style.cursor = "pointer";
+linkedin.addEventListener("click", (e) => {
+     window.open("https://www.linkedin.com/in/maximilian-h%C3%BCtter-61a016262/");
 }) 
 
-document.getElementById("insta").style.cursor = "pointer";
-insta.addEventListener("click", (e) => {
-    window.open("https://www.instagram.com/maxl.htr/");
-}) 
+// document.getElementById("wfod2").style.cursor = "pointer";
+// wfod2.addEventListener("click", (e) => {
+//     window.open("https://wfo-delivery.vercel.app/");
+// }) 
 
-document.getElementById("wfod").style.cursor = "pointer";
-wfod.addEventListener("click", (e) => {
-    window.open("https://wfo-delivery.vercel.app/");
-}) 
-
-document.getElementById("wfod2").style.cursor = "pointer";
-wfod2.addEventListener("click", (e) => {
-    window.open("https://wfo-delivery.vercel.app/");
-}) 
-
-document.getElementById("box").style.cursor = "pointer";
-document.getElementById("box1").style.cursor = "pointer";
-document.getElementById("box2").style.cursor = "pointer";
-document.getElementById("box3").style.cursor = "pointer";
-
-//HTML TAGS TEXT TITLE// 
-/*
-let tag1 = document.getElementsByClassName("customTag")[0]
-tag1.innerText = "<name>"
-tag1.style.color = 'rgb(216, 84, 103)'
-
-let tag2 = document.getElementsByClassName("customTag")[1]
-tag2.innerText = "</name>"
-tag2.style.color = "rgb(216, 84, 103)"
-*/
+// document.getElementById("box").style.cursor = "pointer";
+// document.getElementById("box1").style.cursor = "pointer";
+// document.getElementById("box2").style.cursor = "pointer";
+// document.getElementById("box3").style.cursor = "pointer";
 
 //BLINKY BLINKY//
 
@@ -45,4 +34,4 @@ setInterval(() => {
         document.getElementById("cursor").style.opacity = 1;
     }
     visibleCursor = !visibleCursor
-}, 50);
+}, 400);
