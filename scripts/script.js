75dce45d5d38e0832e2adb42fd69d2e9c3965d24
Maximilian <93908181+Maxldaxl1337@users.noreply.github.com>
2023-11-30 12:45:06 +0100

    i dunno

--- scripts/script.js
+++ scripts/script.js
@@ -1,19 +1,19 @@
 
  // SOCIAL MEDIA LINKS
- document.getElementById("github").style.cursor = "pointer";
- github.addEventListener("click", (e) => {
-     window.open("https://github.com/Maxldaxl1337");
- }) 
+//  document.getElementById("github").style.cursor = "pointer";
+//  github.addEventListener("click", (e) => {
+//      window.open("https://github.com/Maxldaxl1337");
+//  }) 
 
-document.getElementById("instagram").style.cursor = "pointer";
-instagram.addEventListener("click", (e) => {
-     window.open("https://www.instagram.com/maxl.htr/");
- }) 
+// document.getElementById("instagram").style.cursor = "pointer";
+// instagram.addEventListener("click", (e) => {
+//      window.open("https://www.instagram.com/maxl.htr/");
+//  }) 
 
-document.getElementById("linkedin").style.cursor = "pointer";
-linkedin.addEventListener("click", (e) => {
-     window.open("https://www.linkedin.com/in/maximilian-h%C3%BCtter-61a016262/");
-}) 
+// document.getElementById("linkedin").style.cursor = "pointer";
+// linkedin.addEventListener("click", (e) => {
+//      window.open("https://www.linkedin.com/in/maximilian-h%C3%BCtter-61a016262/");
+// }) 
 
 //BLINKY BLINKY//
 let visibleCursor = false
